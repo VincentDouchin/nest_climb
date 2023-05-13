0bbddcf9bc0736d2d6bca6fc442f6357d2821f51
Vincent Douchin <vincent.douchin@gmail.com>
2023-05-13 14:13:13 +0200

    add web build

--- scripts/web_build.sh
+++ scripts/web_build.sh
@@ -1,3 +1,5 @@
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./dist/ --target web ./target/wasm32-unknown-unknown/release/nest_climb.wasm
 cp -a ./assets/. ./dist/assets/
+git commit -m build -- dist
+git push origin main
