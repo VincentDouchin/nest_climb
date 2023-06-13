63232cfd6e7eb1b8629b950bbf6a2caf91c78fd5
Vincent Douchin <vincent.douchin@gmail.com>
2023-06-13 08:12:56 +0200

    add web project

---
+++ NestClimb/vite.confif.ts
@@ -0,0 +1,58 @@
+import { VitePWA } from 'vite-plugin-pwa'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => {
+
+	const config: UserConfig = {
+		plugins: [
+			VitePWA({
+				registerType: 'autoUpdate',
+				injectRegister: 'auto',
+				includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+				injectManifest: {
+					globPatterns: ['**/*.{js,html,wasm}', './assets/images/*.*', './assets/map/Arenas/*.png', './assets/sounds/*.*', './assets/UI/*.*'],
+				},
+				manifest: {
+					start_url: 'index.html?fullscreen=true',
+					display: 'fullscreen',
+					orientation: 'landscape',
+					name: 'Dungeon Survivor',
+					short_name: 'DungeonSurvivor',
+					description: 'Dungeon Survivor',
+					theme_color: '#000000',
+					icons: [
+						{
+							src: 'pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png',
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any',
+						},
+					],
+				},
+				devOptions: {
+					enabled: true,
+				},
+			}),
+		],
+		base: '/',
+		build: {
+			target: 'esnext',
+			rollupOptions: {
+				output: {
+					entryFileNames: 'assets/[name].js',
+				},
+			},
+		},
+
+
+	}
+
+	return config
+})
