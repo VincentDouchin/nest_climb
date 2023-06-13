f4059ee3fdbc4744b17b741b92baecf8a119c2a1
Vincent Douchin <vincent.douchin@gmail.com>
2023-06-13 08:44:11 +0200

    update web build

--- scripts/web_build.sh
+++ scripts/web_build.sh
@@ -1,6 +1,9 @@
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./NestClimb/assets --target web ./target/wasm32-unknown-unknown/release/nest_climb.wasm
+wasm-bindgen --out-dir ./NestClimb/src/wasm --target web ./target/wasm32-unknown-unknown/release/nest_climb.wasm
 cp -a ./assets/. ./NestClimb/public/assets/
-git add ./NestClimb/public/assets/*
-git commit -m build -- NestClimb/*
+
+git add ./NestClimb/src/*
+git add ./NestClimb/public/*
+git commit -m build -- NestClimb/public/* 
+git commit -m build -- NestClimb/src/*
 git push origin main
